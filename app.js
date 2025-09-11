ae40b2c793d38c79f2027dfb46cf6d7b9f71ac66
ngoywangoy.joshua <ngoywangoy.joshua@outlook.com>
2025-09-11 08:50:54 +0200

    Commit#2

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('TP CIEL');
+exp.get('/', function (req, res) {
+    console.log(‘Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+}); 
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+}); 
+
+    exp.listen(80, function () {
+        console.log('Serveur en ecoute');
+    }); 
