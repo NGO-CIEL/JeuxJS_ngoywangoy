7ebc7886bd97a4903f2143105ad49a0c6ce8d37c
ngoywangoy.joshua <ngoywangoy.joshua@outlook.com>
2025-09-11 09:14:01 +0200

    Commit #3

--- app.js
+++ app.js
@@ -1,15 +1,82 @@
 'use strict';
 
-exp.get('/', function (req, res) {
-    console.log(‘Reponse a un client'); 
-    res.sendFile(__dirname + '/www/index.html');
+/*  *************** serveur WebSocket express *********************   */
+// 
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés 
+exp.ws('/echo', function (ws, req) {
+
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        ws.send(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
+
 }); 
 
-exp.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Erreur serveur express');
+/*  ****** Serveur web et WebSocket en ecoute sur le port 80  ********   */
+//  
+var portServ = 80;
+exp.listen(portServ, function () {
+    console.log('Serveur en ecoute');
 }); 
 
-    exp.listen(80, function () {
-        console.log('Serveur en ecoute');
-    }); 
+?  Dans le script echo.js ajouter les variables pour la WebSocket en mettant en dur votre 
+adresse IP de poste:
+var ipServeur = '172.17.50.xxx';     // Adresse ip du serveur  
+var ws;                             // Variable pour l'instance de la WebSocket. 
+ 
+?  Après le chargement de la page(événement) vérifier si le navigateur est compatible avec les
+WebSockets, ce qui est le cas maintenant pour la plupart des navigateurs:
+window.onload = function () {
+    if (TesterLaCompatibilite()) {
+        ConnexionAuServeurWebsocket();
+    }
+    ControleIHM();
+}
+
+function TesterLaCompatibilite() {
+    let estCompatible = true;
+    if (!('WebSocket' in window)) {
+        window.alert('WebSocket non supporté par le navigateur');
+        estCompatible = false;
+    }
+    return estCompatible; 
+ 
+?  La connexion au serveur WebSocket est réalisée lors de l’instanciation de la WebSocket,
+        ensuite on traite les événements associés:
+
+    /*  ***************** Connexion au serveur WebSocket ********************   */
+    // 
+    function ConnexionAuServeurWebsocket() {
+        ws = new WebSocket('ws://' + ipServeur + '/echo');
+
+        ws.onclose = function (evt) {
+            window.alert('WebSocket close');
+        };
+
+        ws.onopen = function () {
+            console.log('WebSocket open');
+        };
+
+        ws.onmessage = function (evt) {
+            document.getElementById('messageRecu').value = evt.data;
+        };
+    }
+
+    function ControleIHM() {
+        document.getElementById('Envoyer').onclick = BPEnvoyer;
+    }
+
+    function BPEnvoyer() {
+        ws.send(document.getElementById('messageEnvoi').value);
+    } 
